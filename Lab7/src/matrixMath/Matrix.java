53a4a2ac67e6c2742ce49351f97bbc961e54d2ef
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-13 22:44:09 -0500

    Initial upload
    Initial upload of the new Java project. Includes a matrixMath package containing a main.java file and an initialized Matrix.java file.

---
+++ Lab7/src/matrixMath/Matrix.java
@@ -0,0 +1,19 @@
+package matrixMath;
+
+public class Matrix {
+	int n = 0;
+	
+	double determinant() {
+		return 0.0;
+	}
+	
+	Matrix inverse() {
+		Matrix inv = new Matrix();
+		return inv;
+	}
+	
+	Matrix subMatrix(int r, int c) {
+		Matrix sub = new Matrix();
+		return sub;
+	}
+}
