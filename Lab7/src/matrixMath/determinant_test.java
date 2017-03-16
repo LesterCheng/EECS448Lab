2ddf0410f3f25d99c1e1efa520de4fd03c751e25
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-15 23:57:10 -0500

    Add Unit Test
    Added an empty Unit Test for the determinant function.

---
+++ Lab7/src/matrixMath/determinant_test.java
@@ -0,0 +1,14 @@
+package matrixMath;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class determinant_test {
+
+	@Test
+	public void testDeterminant() {
+		fail("Not yet implemented");
+	}
+
+}
