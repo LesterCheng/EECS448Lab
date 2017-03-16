402f7b908f7ed889009070ca45b0891c41f8f885
Lester Cheng <lester.cs.cheng@gmail.com>
2017-03-16 00:03:54 -0500

    Define test cases
    Created empty test cases for determinant Unit Tests.

--- Lab7/src/matrixMath/determinant_test.java
+++ Lab7/src/matrixMath/determinant_test.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class determinant_test {
 
 	@Test
-	public void testDeterminant() {
-		fail("Not yet implemented");
+	public void testDeterminantSize1() { //Test case for n=1
+		
+	}
+	
+	@Test
+	public void testDeterminantSize2() { //Test case for n=2
+		
+	}
+	
+	@Test
+	public void testDeterminant() {	//Test case for n>2
+		
 	}
 
 }
