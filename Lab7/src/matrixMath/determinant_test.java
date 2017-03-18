1304ab031c122d8fe6697c389aea29e4f7573328
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-17 21:25:11 -0500

    Complete Unit Tests
    Completed Unit tests for determinant_test.

--- Lab7/src/matrixMath/determinant_test.java
+++ Lab7/src/matrixMath/determinant_test.java
@@ -7,18 +7,63 @@ import org.junit.Test;
 public class determinant_test {
 
 	@Test
-	public void testDeterminantSize1() { //Test case for n=1
-		
+	public void testDeterminantSize1() { // Test case for n=1
+		// Initialize value of n
+		int n = 1;
+
+		// Create new instance of Matrix
+		Matrix test = new Matrix(n);
+
+		// Fill matrix with values
+		double[][] input = new double[n][n];
+		input[0][0] = 1;
+		test.setData(input);
+
+		assertEquals(1.0, test.determinant(), 0);
 	}
-	
+
 	@Test
-	public void testDeterminantSize2() { //Test case for n=2
-		
+	public void testDeterminantSize2() { // Test case for n=2
+		// Initialize value of n
+		int n = 2;
+
+		// Create new instance of Matrix
+		Matrix test = new Matrix(n);
+
+		// Fill matrix with values
+		double[][] input = new double[n][n];
+		input[0][0] = 3;
+		input[0][1] = 8;
+		input[1][0] = 4;
+		input[1][1] = 6;
+		test.setData(input);
+
+		assertEquals(-14.0, test.determinant(), 0);
 	}
-	
+
 	@Test
-	public void testDeterminant() {	//Test case for n>2
+	public void testDeterminant() { // Test case for n>2
+		// Initialize value of n
+		int n = 3;
+
+		// Create new instance of Matrix
+		Matrix test = new Matrix(n);
+
+		// Fill matrix with values
+		double[][] input = new double[n][n];
+		input[0][0] = 6;
+		input[0][1] = 1;
+		input[0][2] = 1;
+		input[1][0] = 4;
+		input[1][1] = -2;
+		input[1][2] = 5;
+		input[2][0] = 2;
+		input[2][1] = 8;
+		input[2][2] = 7;
 		
+		test.setData(input);
+
+		assertEquals(-306.0, test.determinant(), 0);
 	}
 
 }
