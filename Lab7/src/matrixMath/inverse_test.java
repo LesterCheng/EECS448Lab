2719ae5a823328bc9890eaf171b7b31b904aa0ad
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-17 21:28:41 -0500

    Add Unit Test
    Added unit test for inverse() method.

---
+++ Lab7/src/matrixMath/inverse_test.java
@@ -0,0 +1,14 @@
+package matrixMath;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class inverse_test {
+
+	@Test
+	public void testInverse() {
+		fail("Not yet implemented");
+	}
+
+}
