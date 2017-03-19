81d5dcbbb56ea67da8af31501bb33c99c4b45ff1
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-19 11:19:53 -0500

    Add Unit Test
    Added a submatrix(int, int) unit test: submatrix_test.

---
+++ Lab7/src/matrixMath/submatrix_test.java
@@ -0,0 +1,14 @@
+package matrixMath;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class submatrix_test {
+
+	@Test
+	public void testSubMatrix() {
+		fail("Not yet implemented");
+	}
+
+}
