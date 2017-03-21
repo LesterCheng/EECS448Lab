f1b763779e4648445a48e6ff2515f168e4ce15fc
Lester Cheng <LesterCheng@users.noreply.github.com>
2017-03-20 19:36:41 -0500

    Complete Unit Test
    Completed submatrix_test Unit test. To test each submatrix case, I compared each actual case to its expected case and stored the result of this evaluation in a boolean array. I then loop through the boolean array and throw a flag if there's a "false" entry. I then pass this flag into assertTrue() which gives me a pass or fail result for the whole unit test.

--- Lab7/src/matrixMath/submatrix_test.java
+++ Lab7/src/matrixMath/submatrix_test.java
@@ -2,6 +2,8 @@ package matrixMath;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class submatrix_test {
@@ -26,21 +28,118 @@ public class submatrix_test {
 
 		// Fill Matrix test with values
 		test.setData(input);
-		
-		double[][] output = test.subMatrix(0, 0).getData();
-		
-		// Create expected matrices
-		double[][] temp = new double[10][10];
-		
-		// submatrix(0,0)
-		temp[0][0] = -2;
-		temp[0][1] = 5;
-		temp[1][0] = 8;
-		temp[1][1] = 7;
-		
-		assertArrayEquals(temp, output);
-		
-		
-	}
 
+		// case1: submatrix(0,0)
+		// Create expected matrices
+		double[][] case1 = new double[10][10];
+		case1[0][0] = -2;
+		case1[0][1] = 5;
+		case1[1][0] = 8;
+		case1[1][1] = 7;
+		// Actual values
+		double[][] actual1 = test.subMatrix(0, 0).getData();
+
+		// case2: submatrix(0,1)
+		// Create expected matrices
+		double[][] case2 = new double[10][10];
+		case2[0][0] = 4;
+		case2[0][1] = 5;
+		case2[1][0] = 2;
+		case2[1][1] = 7;
+		// Actual values
+		double[][] actual2 = test.subMatrix(0, 1).getData();
+
+		// case3: submatrix(0,2)
+		// Create expected matrices
+		double[][] case3 = new double[10][10];
+		case3[0][0] = 4;
+		case3[0][1] = -2;
+		case3[1][0] = 2;
+		case3[1][1] = 8;
+		// Actual values
+		double[][] actual3 = test.subMatrix(0, 2).getData();
+
+		// case4: submatrix(1,0)
+		// Create expected matrices
+		double[][] case4 = new double[10][10];
+		case4[0][0] = 1;
+		case4[0][1] = 1;
+		case4[1][0] = 8;
+		case4[1][1] = 7;
+		// Actual values
+		double[][] actual4 = test.subMatrix(1, 0).getData();
+
+		// case5: submatrix(1,1)
+		// Create expected matrices
+		double[][] case5 = new double[10][10];
+		case5[0][0] = 6;
+		case5[0][1] = 1;
+		case5[1][0] = 2;
+		case5[1][1] = 7;
+		// Actual values
+		double[][] actual5 = test.subMatrix(1, 1).getData();
+
+		// case6: submatrix(1,2)
+		// Create expected matrices
+		double[][] case6 = new double[10][10];
+		case6[0][0] = 6;
+		case6[0][1] = 1;
+		case6[1][0] = 2;
+		case6[1][1] = 8;
+		// Actual values
+		double[][] actual6 = test.subMatrix(1, 2).getData();
+
+		// case7: submatrix(2,0)
+		// Create expected matrices
+		double[][] case7 = new double[10][10];
+		case7[0][0] = 1;
+		case7[0][1] = 1;
+		case7[1][0] = -2;
+		case7[1][1] = 5;
+		// Actual values
+		double[][] actual7 = test.subMatrix(2, 0).getData();
+
+		// case8: submatrix(2,1)
+		// Create expected matrices
+		double[][] case8 = new double[10][10];
+		case8[0][0] = 6;
+		case8[0][1] = 1;
+		case8[1][0] = 4;
+		case8[1][1] = 5;
+		// Actual values
+		double[][] actual8 = test.subMatrix(2, 1).getData();
+
+		// case9: submatrix(2,2)
+		// Create expected matrices
+		double[][] case9 = new double[10][10];
+		case9[0][0] = 6;
+		case9[0][1] = 1;
+		case9[1][0] = 4;
+		case9[1][1] = -2;
+		// Actual values
+		double[][] actual9 = test.subMatrix(2, 2).getData();
+
+		//Compare expected and actual matrices
+		boolean[] actualArr = new boolean[9];
+		actualArr[0] = Arrays.deepEquals(case1, actual1);
+		actualArr[1] = Arrays.deepEquals(case2, actual2);
+		actualArr[2] = Arrays.deepEquals(case3, actual3);
+		actualArr[3] = Arrays.deepEquals(case4, actual4);
+		actualArr[4] = Arrays.deepEquals(case5, actual5);
+		actualArr[5] = Arrays.deepEquals(case6, actual6);
+		actualArr[6] = Arrays.deepEquals(case7, actual7);
+		actualArr[7] = Arrays.deepEquals(case8, actual8);
+		actualArr[8] = Arrays.deepEquals(case9, actual9);
+
+		boolean testFlag = true;
+		int caseCount = 1;
+		for (boolean i : actualArr) {
+			if (i == false) {
+				testFlag = false;
+				System.out.println("Error with case: " + caseCount);
+			}
+			caseCount++;
+		}
+		assertTrue(testFlag);
+	}
 }
